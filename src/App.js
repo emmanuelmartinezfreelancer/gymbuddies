825e8f914aaf06173618d1c70967b1e621a5e827
Emmanuel Martínez <emmanuel@neoncaron.com>
2023-01-03 08:41:43 -0600

    JSON Schemas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Home } from "./components/Home";
+import { Profile } from "./components/Profile"
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/authContext";
+import { HomeJuez } from "./components/HomeJuez";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black h-screen text-teal-400 flex">
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={
+              <ProtectedRoute>
+                  <Profile />
+              </ProtectedRoute>
+
+          }/>
+          <Route path="/home-juez" element={
+              <ProtectedRoute>
+                  <HomeJuez />
+              </ProtectedRoute>
+
+          }/>
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
